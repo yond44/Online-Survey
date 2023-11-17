68927ac8abed0ce7ed765a13b7463391efc54f94
yond44 <yondaekodirman@gmail.com>
2023-11-18 03:00:14 +0700

    test lagi

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios'
 
-axios.defaults.withCredentials = true;
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
